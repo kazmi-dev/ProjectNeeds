6696b1a872d6d76e1c828f3759e51ee62195ee38
Syed Turaab Haider Kazmi <154650465+kazmi-dev@users.noreply.github.com>
2024-02-29 17:24:24 +0500

    Add files via upload

---
+++ room_db/Database.kt
@@ -0,0 +1,12 @@
+package com.tenx.backgroundremover.mvvm.room_db
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.tenx.backgroundremover.mvvm.database.room.ItemsModelDao
+import com.tenx.backgroundremover.mvvm.models.ItemsModel
+
+//change to your model class
+@Database(entities = [yourModelClass::class], version = 1 , exportSchema = true)
+abstract class Database: RoomDatabase() {
+    //place your dao here i.e:  ( abstract fun myDao(): MyDao )
+}
---
+++ room_db/RoomDatabaseModule.kt
@@ -0,0 +1,35 @@
+package com.tenx.backgroundremover.mvvm.room_db
+
+import android.content.Context
+import androidx.room.Room
+import com.tenx.backgroundremover.mvvm.database.room.Database
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RoomDatabaseModule {
+
+    private const val DATABASE_NAME = "my_database"
+
+    @Provides
+    @Singleton
+    fun providesDatabaseInstance(
+        @ApplicationContext context: Context
+    ): Database {
+        return Room.databaseBuilder(
+            context,
+            Database::class.java,
+            DATABASE_NAME
+        )
+            .fallbackToDestructiveMigration()
+            .build()
+    }
+
+    //Dao will be placed here
+
+}
