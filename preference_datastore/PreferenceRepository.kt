6208318c3a4abcc226cb2e3e462b2e96ecd668b7
Syed Turaab Haider Kazmi <154650465+kazmi-dev@users.noreply.github.com>
2024-05-31 12:16:02 +0500

    PreferenceRepository.kt

--- preference_datastore/PreferenceRepository.kt
+++ preference_datastore/PreferenceRepository.kt
@@ -1,4 +1,3 @@
-package com.tenx.translator.mvvm.preference_datastore
 
 import android.util.Log
 import androidx.datastore.core.DataStore
@@ -9,7 +8,9 @@ import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.floatPreferencesKey
 import androidx.datastore.preferences.core.intPreferencesKey
 import androidx.datastore.preferences.core.stringPreferencesKey
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.withContext
 import java.io.IOException
 import javax.inject.Inject
 
@@ -22,65 +23,106 @@ class PreferenceRepository@Inject constructor(
     }
 
     //save and get boolean
-    suspend fun saveBoolean(key: String, value: Boolean){
+    suspend fun saveBoolean(key: String, value: Boolean) = withContext(Dispatchers.IO){
         try {
             dataStore.edit { pref ->
                 pref[booleanPreferencesKey(key)] = value
             }
         }catch (e: IOException){
-            Log.e(DATASTORE_LOG, "boolean: cant save file.", e)
+            Log.e(DATASTORE_LOG, "Boolean: cant save.", e)
+        }catch (e:Exception){
+            Log.e(DATASTORE_LOG, "Boolean: cant save.", e)
         }
     }
+    
     suspend fun getBoolean(key: String, defaultValue: Boolean): Boolean{
-        return dataStore.data.first()[booleanPreferencesKey(key)]?: defaultValue
+        return withContext(Dispatchers.IO){
+            dataStore.data.first()[booleanPreferencesKey(key)]?: defaultValue
+        }
     }
 
 
 
     //save and get String
-    suspend fun saveString(key: String, value: String){
-        dataStore.edit {pref->
-            pref[stringPreferencesKey(key)] = value
+    suspend fun saveString(key: String, value: String) = withContext(Dispatchers.IO){
+        try {
+            dataStore.edit {pref->
+                pref[stringPreferencesKey(key)] = value
+            }
+        }catch (e: IOException){
+            Log.e(DATASTORE_LOG, "String: cant save.", e)
+        }catch (e:Exception){
+            Log.e(DATASTORE_LOG, "String: cant save.", e)
         }
     }
+    
     suspend fun getString(key: String, defaultValue: String): String{
-        return dataStore.data.first()[stringPreferencesKey(key)]?: defaultValue
+        return withContext(Dispatchers.IO){
+            dataStore.data.first()[stringPreferencesKey(key)]?: defaultValue
+        }
     }
 
 
 
     //save and get Int
-    suspend fun saveInt(key: String, value: Int){
-        dataStore.edit {pref->
-            pref[intPreferencesKey(key)] = value
+    suspend fun saveInt(key: String, value: Int) = withContext(Dispatchers.IO){
+        try {
+            dataStore.edit {pref->
+                pref[intPreferencesKey(key)] = value
+            }
+        }catch (e: IOException){
+            Log.e(DATASTORE_LOG, "Int: cant save.", e)
+        }catch (e:Exception){
+            Log.e(DATASTORE_LOG, "Int: cant save.", e)
         }
     }
+    
     suspend fun getInt(key: String, defaultValue: Int): Int{
-        return dataStore.data.first()[intPreferencesKey(key)]?: defaultValue
+        return withContext(Dispatchers.IO){
+            dataStore.data.first()[intPreferencesKey(key)]?: defaultValue
+        }
     }
 
 
 
     //save and get Double
-    suspend fun saveDouble(key: String, value: Double){
-        dataStore.edit {pref->
-            pref[doublePreferencesKey(key)] = value
+    suspend fun saveDouble(key: String, value: Double) = withContext(Dispatchers.IO){
+        try {
+            dataStore.edit {pref->
+                pref[doublePreferencesKey(key)] = value
+            }
+        }catch (e: IOException){
+            Log.e(DATASTORE_LOG, "Double: cant save.", e)
+        }catch (e:Exception){
+            Log.e(DATASTORE_LOG, "Double: cant save.", e)
         }
     }
+    
     suspend fun getDouble(key: String, defaultValue: Double): Double{
-        return dataStore.data.first()[doublePreferencesKey(key)]?: defaultValue
+        return withContext(Dispatchers.IO){
+            dataStore.data.first()[doublePreferencesKey(key)]?: defaultValue
+        }
     }
 
 
 
     //save and get Float
-    suspend fun saveFloat(key: String, value: Float){
-        dataStore.edit {pref->
-            pref[floatPreferencesKey(key)] = value
+    suspend fun saveFloat(key: String, value: Float) = withContext(Dispatchers.IO){
+        try {
+            dataStore.edit {pref->
+                pref[floatPreferencesKey(key)] = value
+            }
+        }catch (e: IOException){
+            Log.e(DATASTORE_LOG, "Float: cant save.", e)
+        }catch (e:Exception){
+            Log.e(DATASTORE_LOG, "Float: cant save.", e)
         }
     }
+    
     suspend fun getFloat(key: String, defaultValue: Float): Float{
-        return dataStore.data.first()[floatPreferencesKey(key)]?: defaultValue
+        return withContext(Dispatchers.IO){
+            dataStore.data.first()[floatPreferencesKey(key)]?: defaultValue
+        }
     }
 
 }
