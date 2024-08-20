deee377d1568b41c54bcf75316f3379d493356b2
Syed Turaab Haider Kazmi <154650465+kazmi-dev@users.noreply.github.com>
2024-08-20 17:34:43 +0500

    RewardedAdManager.kt

--- ads/RewardedAdManager.kt
+++ ads/RewardedAdManager.kt
@@ -1,9 +1,6 @@
-package com.capra.android.auto.carplay.ads
-
 import android.app.Activity
 import android.content.Context
-import android.util.Log
-import com.capra.android.auto.carplay.R
+import com.cooptech.pdfreader.R
 import com.google.android.gms.ads.AdError
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.FullScreenContentCallback
@@ -14,66 +11,97 @@ import com.google.android.gms.ads.rewarded.RewardedAdLoadCallback
 
 class RewardedAdManager(private val context: Context) {
 
-    private val REWARDED_LOG = "23438578843758934445"
     private var rewardedAd: RewardedAd? = null
-    private var isRewardedAdShowing = false
+    private var isAdLoading: Boolean = false
+    private var rewardedAdUnitId = context.getString(R.string.rewarded_ad)
+    private var isRewardedAdShowing: Boolean = false
+
+    private fun rewardedAdLoadCallback(onLoad: () -> Unit) = object : RewardedAdLoadCallback() {
+        override fun onAdLoaded(rewardedAd: RewardedAd) {
+            isAdLoading = false
+            this@RewardedAdManager.rewardedAd = rewardedAd
+            onLoad()
+        }
+
+        override fun onAdFailedToLoad(error: LoadAdError) {
+            isAdLoading = false
+            rewardedAd = null
+        }
+    }
 
 
-    fun loadRewardedAd(activity: Activity, onRewarded: (RewardItem) -> Unit) {
+    fun setRewardedAdUnitId(rewardedAdUnitId: String) {
+        this.rewardedAdUnitId = rewardedAdUnitId
+    }
+
+    fun loadAndShowRewardedAd(
+        activity: Activity,
+        onRewardedAd: (RewardItem) -> Unit,
+        adStates: (AdStates) -> Unit,
+    ) {
+        //check if reward ad is not null
+        if (rewardedAd != null) {
+            return
+        }
+
+        //check if ad is already loading
+        if (isAdLoading) {
+            return
+        }
+        isAdLoading = true
+
         val adRequest = AdRequest.Builder().build()
+
         RewardedAd.load(
             context,
-            context.getString(R.string.interstitial_rewarded),
+            rewardedAdUnitId,
             adRequest,
-            object: RewardedAdLoadCallback(){
-
-                override fun onAdLoaded(ad: RewardedAd) {
-                    rewardedAd = ad
-                    Log.d(REWARDED_LOG, "rewarded ad loaded")
-                    showRewardedAd(activity, onRewarded)
-                }
-
-                override fun onAdFailedToLoad(loadError: LoadAdError) {
-                    rewardedAd = null
-                    Log.d(REWARDED_LOG, "failed to load rewarded ad: ${loadError.message}")
-                }
+            rewardedAdLoadCallback {
+                showRewardedAdIfAvailable(activity, onRewardedAd, adStates)
             }
         )
     }
 
-    fun showRewardedAd(activity: Activity, onRewarded: (RewardItem) -> Unit){
-        if(rewardedAd == null){
-            Log.d(REWARDED_LOG, "rewarded ad is null")
-            loadRewardedAd(activity, onRewarded)
-            return
-        }
-        if(isRewardedAdShowing){
-            Log.d(REWARDED_LOG, "rewarded ad is showing")
+    private fun showRewardedAdIfAvailable(
+        activity: Activity,
+        onRewarded: (RewardItem) -> Unit,
+        adStates: (AdStates) -> Unit,
+    ) {
+        if (rewardedAd == null) {
+            loadAndShowRewardedAd(activity, onRewarded, adStates)
             return
         }
 
-        rewardedAd?.fullScreenContentCallback = object: FullScreenContentCallback(){
+        if (isRewardedAdShowing) {
+            return
+        }
+
+        rewardedAd?.fullScreenContentCallback = object : FullScreenContentCallback() {
+
             override fun onAdShowedFullScreenContent() {
                 isRewardedAdShowing = true
+                adStates(AdStates.AD_SHOWED)
             }
 
             override fun onAdDismissedFullScreenContent() {
-                Log.d(REWARDED_LOG, "rewarded ad on dismiss")
+                rewardedAd = null
                 isRewardedAdShowing = false
+                adStates(AdStates.AD_DISMISSED)
             }
 
-            override fun onAdFailedToShowFullScreenContent(adError: AdError) {
-                Log.d(REWARDED_LOG, "adError: ${adError.message}")
+            override fun onAdFailedToShowFullScreenContent(error: AdError) {
+                rewardedAd = null
+                isRewardedAdShowing = false
+                adStates(AdStates.AD_FAILED_TO_SHOW)
             }
 
         }
 
-        rewardedAd?.show(activity) {
-            Log.d(REWARDED_LOG, "rewardType: ${it.type}")
-            Log.d(REWARDED_LOG, "rewardAmount: ${it.amount}")
-            onRewarded(it)
+        rewardedAd?.show(activity) {rewardItem->
+            onRewarded(rewardItem)
         }
 
     }
+
 
 }
