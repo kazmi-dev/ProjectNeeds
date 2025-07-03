f918d6616469c6413b63bad7aa83fa8882cd5ffd
Syed Turaab Haider Kazmi <syedzainkazmi78@gmail.com>
2025-07-03 21:57:43 +0500

    Update InterstitialAdManager.kt

--- ads/InterstitialAdManager.kt
+++ ads/InterstitialAdManager.kt
@@ -1,123 +1,164 @@
-package com.cooptech.collagephotoeditor.ads
-
 import android.app.Activity
 import android.content.Context
 import android.util.Log
-import com.cooptech.collagephotoeditor.R
-import com.cooptech.collagephotoeditor.utils.AppSettings
 import com.google.android.gms.ads.AdError
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.FullScreenContentCallback
 import com.google.android.gms.ads.LoadAdError
 import com.google.android.gms.ads.interstitial.InterstitialAd
 import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
+import com.kazmi.dev.gameborse.R
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Inject
+import javax.inject.Singleton
 
-
-/**
----------------------------------------------
-This is Interstitial Ad implementation
----------------------------------------------
- */
-
-class InterstitialAdManager(private val context: Context) {
-
-    private val INTERSTITIAL_LOG = "23865783458934445"
-    private var interstitialAd: InterstitialAd? = null
-    private var isInterstitialAdLoading = false
+@Singleton
+class InterstitialAdManager @Inject constructor(
+    @ApplicationContext private val context: Context
+) {
 
     companion object {
-        var isInterstitialAdShowing = false
-        var clickCount = 0
+        private const val INTERSTITIAL_LOG = "InterstitialAdManager_346298794582"
     }
 
-    //load interstitial ad fun
-    fun loadInterstitialAd() {
+    private var interstitialAd: InterstitialAd? = null
+    private var isAdLoading: Boolean = false
+    private var isAdShowing: Boolean = false
 
-        if (AppSettings.isRemoveAds){
+    fun preloadAd() {
+
+        //check if ad already available
+        if (interstitialAd != null) {
+            Log.d(INTERSTITIAL_LOG, "preloadAd: Ad already available")
             return
         }
 
-        isInterstitialAdLoading = true
+        //check if ad already loading
+        if (isAdLoading) {
+            Log.d(INTERSTITIAL_LOG, "preloadAd: Ad already loading")
+            return
+        }
 
-        //request ad
+        //loadAd
+        val adUnitId = context.getString(R.string.interstitial_ad_id)
+        loadAd(adUnitId) { Log.d(INTERSTITIAL_LOG, "preloadAd: Ad loaded") }
+
+    }
+
+    fun showAdIfAvailable(activity: Activity, callback: ((AdState)-> Unit)? = null) {
+
+        //check if ad is not available to show
+        if (interstitialAd == null) {
+            Log.d(INTERSTITIAL_LOG, "showAdIfAvailable: Ad not available, preloading ad")
+            preloadAd()
+            callback?.invoke(AdState.FAILED_TO_SHOW)
+            return
+        }
+
+        //check if ad is already showing
+        if (isAdShowing) {
+            Log.d(INTERSTITIAL_LOG, "showAdIfAvailable: Ad already showing")
+            return
+        }
+
+        //show ad
+        showAd(activity, isPreloadAfterDismiss = true, callback)
+
+    }
+
+    fun showAdOnDemand(activity: Activity, adUnitId: String, callback: ((AdState) -> Unit)?) {
+
+        //check if ad is already showing
+        if (isAdShowing) {
+            Log.d(INTERSTITIAL_LOG, "showAdOnDemand: Ad already showing")
+            return
+        }
+
+        //check if ad is not available
+        if (interstitialAd == null) {
+            loadAd(adUnitId) {isLoaded->
+                if (isLoaded){
+                    //show ad
+                    attachFullscreenCallback(isPreloadAfterDismiss = false, callback = callback)
+                    isAdShowing = true
+                    interstitialAd?.show(activity)
+                }else{
+                    callback?.invoke(AdState.FAILED_TO_LOAD)
+                }
+            }
+            return
+        }
+
+        //show ad
+        showAd(activity, isPreloadAfterDismiss = false)
+
+    }
+
+    private fun loadAd(adUnitId: String, callback: (Boolean) -> Unit) {
+        isAdLoading = true
+
         val adRequest = AdRequest.Builder().build()
-
-        //load Ad
         InterstitialAd.load(
             context,
-            context.getString(R.string.interstitial_ad_id),
+            adUnitId,
             adRequest,
-            interstitialAdLoadCallback
+            object : InterstitialAdLoadCallback() {
+                override fun onAdLoaded(ad: InterstitialAd) {
+                    Log.d(INTERSTITIAL_LOG, "onAdLoaded: Ad loaded")
+                    isAdLoading = false
+                    interstitialAd = ad
+                    callback(true)
+                }
+
+                override fun onAdFailedToLoad(adLoadError: LoadAdError) {
+                    Log.d(INTERSTITIAL_LOG, "onAdFailedToLoad: ${adLoadError.message}")
+                    isAdLoading = false
+                    interstitialAd = null
+                    callback(false)
+                }
+            }
         )
-
     }
 
-    private val interstitialAdLoadCallback = object : InterstitialAdLoadCallback() {
-
-        override fun onAdLoaded(ad: InterstitialAd) {
-            Log.d(INTERSTITIAL_LOG, "Ad loaded.")
-            isInterstitialAdLoading = false
-            interstitialAd = ad
-        }
-
-        override fun onAdFailedToLoad(error: LoadAdError) {
-            Log.d(INTERSTITIAL_LOG, "Ad failed to load.")
-            isInterstitialAdLoading = false
-            interstitialAd = null
-        }
-
+    private fun showAd(activity: Activity, isPreloadAfterDismiss: Boolean, callback: ((AdState)-> Unit)? = null) {
+        attachFullscreenCallback(isPreloadAfterDismiss, callback)
+        isAdShowing = true
+        interstitialAd?.show(activity)
     }
 
-    private fun isInterstitialAdAvailable(): Boolean {
-        return interstitialAd != null
-    }
-
-    private fun showInterstitialAdIfAvailable(currentActivity: Activity, callback: (AdState) -> Unit) {
-
-        if (isInterstitialAdShowing) {
-            Log.d(INTERSTITIAL_LOG, "Ad already showing or Loading.")
-            callback(AdState.IS_NOT_LOADED)
-            return
-        }
-
-        if (!isInterstitialAdAvailable()) {
-            Log.d(INTERSTITIAL_LOG, "Ad not loaded.")
-            loadInterstitialAd()
-            callback(AdState.IS_NOT_LOADED)
-            return
-        }
-
-        //Interstitial ad load callback
+    private fun attachFullscreenCallback(
+        isPreloadAfterDismiss: Boolean,
+        callback: ((AdState) -> Unit)?
+    ) {
+        //callback for ad showing
         interstitialAd?.fullScreenContentCallback = object : FullScreenContentCallback() {
 
             override fun onAdShowedFullScreenContent() {
-                Log.d(INTERSTITIAL_LOG, "Ad Showing.")
-                callback(AdState.IS_SHOWED)
+                Log.d(INTERSTITIAL_LOG, "onAdShowedFullScreenContent: Ad showed")
+                callback?.invoke(AdState.SHOWED)
             }
 
             override fun onAdDismissedFullScreenContent() {
-                Log.d(INTERSTITIAL_LOG, "Ad dismissed.")
+                Log.d(
+                    INTERSTITIAL_LOG,
+                    "onAdDismissedFullScreenContent: Ad dismissed preloading ad"
+                )
                 interstitialAd = null
-                isInterstitialAdShowing = false
-                loadInterstitialAd()
-                callback(AdState.IS_DISMISSED)
+                isAdShowing = false
+                if (isPreloadAfterDismiss) preloadAd()
+                callback?.invoke(AdState.DISMISSED)
             }
 
-            override fun onAdFailedToShowFullScreenContent(error: AdError) {
-                Log.d(INTERSTITIAL_LOG, "Ad failed to load: ${error.message}")
-                isInterstitialAdShowing = false
-                callback(AdState.IS_FAILED_TO_LOAD)
+            override fun onAdFailedToShowFullScreenContent(adShowError: AdError) {
+                Log.d(
+                    INTERSTITIAL_LOG,
+                    "onAdFailedToShowFullScreenContent: Ad failed to show ${adShowError.message}"
+                )
+                interstitialAd = null
+                isAdShowing = false
+                callback?.invoke(AdState.FAILED_TO_SHOW)
             }
-
         }
-        isInterstitialAdShowing = true
-        interstitialAd?.show(currentActivity)
-
     }
-
-    fun showInterstitialAd(currentActivity: Activity, callback: (AdState) -> Unit){
-        showInterstitialAdIfAvailable(currentActivity, callback)
-    }
-
 
 }
