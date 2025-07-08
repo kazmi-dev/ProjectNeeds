f0040c9fdc8bd609c54ab2548899c0f8679b95c6
Syed Turaab Haider Kazmi <syedzainkazmi78@gmail.com>
2025-07-08 15:43:04 +0500

    Create LiveClassWrapper.kt

---
+++ util/LiveClassWrapper.kt
@@ -0,0 +1,10 @@
+class LiveClassWrapper<out T>(private val content: T) {
+    private var hasBeenHandled = false
+    fun getContentIfNotHandled(): T? {
+        return if (hasBeenHandled) null else {
+            hasBeenHandled = true
+            content
+        }
+    }
+    fun peekContent(): T = content
+}
