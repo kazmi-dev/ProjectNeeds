feec11b7b825c943d91a16cf9a566480a4f109b0
Syed Turaab Haider Kazmi <syedzainkazmi78@gmail.com>
2025-07-16 16:41:00 +0500

    Update BannerAd.kt

--- ads/BannerAd.kt
+++ ads/BannerAd.kt
@@ -1,146 +1,212 @@
-package com.cooptech.collagephotoeditor.ads
-
+import android.app.Activity
 import android.content.Context
-import android.graphics.drawable.ColorDrawable
 import android.os.Build
+import android.os.Build.VERSION.SDK_INT
 import android.os.Bundle
 import android.util.DisplayMetrics
 import android.util.Log
-import android.view.View
-import android.view.WindowManager
 import android.widget.FrameLayout
 import android.widget.TextView
-import androidx.annotation.RequiresApi
-import com.cooptech.collagephotoeditor.R
 import com.google.ads.mediation.admob.AdMobAdapter
 import com.google.android.gms.ads.AdListener
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.AdSize
 import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.LoadAdError
+import dagger.hilt.android.qualifiers.ApplicationContext
 import java.util.UUID
+import javax.inject.Inject
+import javax.inject.Singleton
 
-/**
------------------------------------------------
-This is Anchored Adaptive banner Implementation
------------------------------------------------
- */
-
-class BannerAd(
-    private val context: Context,
-    private val adViewContainer: FrameLayout,
-    private val loadingView: TextView? = null,
-    private val isAnchor: Boolean = true,
-    private val isCollapsible: Boolean = false
+@Singleton
+class BannerAdManager@Inject constructor(
+    @ApplicationContext private val context: Context
 ) {
 
-    private lateinit var adView: AdView
-    private var initialLayoutComplete = false
-
-    private lateinit var windowManager: WindowManager
-
-    init {
-        loadBannerAd()
+    companion object {
+        private const val BANNER_LOG = "BannerAdManager_346298794582"
     }
+    private lateinit var adSize: AdSize
 
-    //get banner ad size
-    private val adSize: AdSize
-        get() {
-            return if (isAnchor){
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-                    getAdSizeForAndroidAboveAndEqualApi30()
-                } else {
-                    getAdSizeForAndroidBelowApi30()
-                }
-            }else{
-                AdSize.BANNER
-            }
-        }
+    private val adViewList: MutableList<AdView> = mutableListOf()
 
-    @RequiresApi(Build.VERSION_CODES.R)
-    private fun getAdSizeForAndroidAboveAndEqualApi30(): AdSize {
-        val windowMetrics = windowManager.currentWindowMetrics
-        val bounds = windowMetrics.bounds
-        var adWidthPixels = adViewContainer.width.toFloat()
-
-        // If the ad hasn't been laid out, default to the full screen width.
-        if (adWidthPixels == 0f) {
-            adWidthPixels = bounds.width().toFloat()
-        }
-
-        val density = context.resources.displayMetrics.density
-        val adWidth = (adWidthPixels / density).toInt()
-
-        return AdSize.getCurrentOrientationAnchoredAdaptiveBannerAdSize(context, adWidth)
-    }
-
-    private fun getAdSizeForAndroidBelowApi30(): AdSize {
+    private fun getAdSize(activity: Activity, adViewContainer: FrameLayout?): AdSize {
+        val windowManager = activity.windowManager
         val displayMetrics = DisplayMetrics()
-        windowManager.defaultDisplay.getMetrics(displayMetrics)
+        var density = activity.resources.displayMetrics.density
 
-        var adWidthPixels = adViewContainer.width.toFloat()
-
-        // If the ad hasn't been laid out, default to the full screen width.
-        if (adWidthPixels == 0f) {
-            adWidthPixels = displayMetrics.widthPixels.toFloat()
-        }
-
-        val density = displayMetrics.density
-        val adWidth = (adWidthPixels / density).toInt()
-
-        return AdSize.getCurrentOrientationAnchoredAdaptiveBannerAdSize(context, adWidth)
-    }
-
-    private fun loadBannerAd() {
-        windowManager = context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
-        adView = AdView(context)
-        //loading customization
-        loadingView?.layoutParams?.height = adSize.getHeightInPixels(context)
-        loadingView?.background = ColorDrawable(context.resources.getColor(R.color.text_gray, null))
-
-        adViewContainer.addView(adView)
-        adViewContainer.viewTreeObserver.addOnGlobalLayoutListener {
-            if (!initialLayoutComplete) {
-                initialLayoutComplete = true
-                //load banner ad fun
-                loadBanner(loadingView)
+        val currentScreenWidthInPx = adViewContainer?.width.takeIf { it != 0 }?.toFloat() ?: run {
+            if (SDK_INT >= Build.VERSION_CODES.R) {
+                windowManager.currentWindowMetrics.bounds.width().toFloat()
+            } else {
+                windowManager.defaultDisplay.getMetrics(displayMetrics)
+                density = displayMetrics.density
+                displayMetrics.widthPixels.toFloat()
             }
         }
+
+        val adWidth = (currentScreenWidthInPx / density).toInt()
+        return AdSize.getCurrentOrientationAnchoredAdaptiveBannerAdSize(activity, adWidth)
     }
 
-//    private fun convertDpToPx(dp: Int): Int {
-//        val density = context.resources.displayMetrics.density
-//        return (dp * density).toInt()
-//    }
+    /******************
+        load and show
+    ********************/
+    fun loadAndShowAd(
+         activity: Activity,
+         adUnitId: String,
+         adViewContainer: FrameLayout,
+         loadingView: TextView?,
+         isCollapsible: Boolean = false,
+         isCollapsibleAtBottom: Boolean = true
+    ) {
 
-    private fun loadBanner(loadingView: TextView? =null) {
-        //set banner ad id
-        adView.adUnitId = if (isCollapsible) context.getString(R.string.collapsive_banner_ad_id) else context.getString(R.string.banner_ad_id)
+        //initialize AD View
+        val adView = AdView(context)
 
-        //set banner ad size
+        //Get Ad Size
+        adSize = getAdSize(activity, adViewContainer)
+
+        //Set size of banner ad viewContainer
+        adViewContainer.apply {
+            layoutParams.apply {
+                width = adSize.getWidthInPixels(activity)
+                height = adSize.getHeightInPixels(activity)
+            }
+            requestLayout()
+        }
+        loadBannerAd(
+            adView,
+            adUnitId,
+            adViewContainer,
+            loadingView,
+            isCollapsible,
+            isCollapsibleAtBottom
+        )
+
+    }
+
+    private fun loadBannerAd(
+        adView: AdView,
+        adUnitId: String,
+        adViewContainer: FrameLayout,
+        loadingView: TextView?,
+        isCollapsible: Boolean = false,
+        isCollapsibleAtBottom: Boolean = true
+    ) {
+        //Set Banner Ad Size
         adView.setAdSize(adSize)
 
-        //request banner ad
-        val adRequestBuilder: AdRequest = if (isCollapsible){
+        //Set Ad Unit Id
+        adView.adUnitId = adUnitId
+
+        //attach view to container
+        adViewContainer.removeAllViews()
+        adViewContainer.addView(adView)
+
+        //Request Ad
+        val adRequest: AdRequest = if (isCollapsible) {
             val extras = Bundle()
-            extras.putString("collapsible", "bottom")
+            extras.putString("collapsible", if (isCollapsibleAtBottom) "bottom" else "top")
             extras.putString("collapsible_request_id", UUID.randomUUID().toString())
             AdRequest.Builder()
                 .addNetworkExtrasBundle(AdMobAdapter::class.java, extras)
                 .build()
-        }else{
+        } else {
             AdRequest.Builder().build()
         }
 
-        //load banner ad
-        adView.loadAd(adRequestBuilder)
+        //load Ad
+        adView.loadAd(adRequest)
 
-        adView.adListener = object : AdListener() {
+        //Ad load and show Listeners
+        adView.adListener = object: AdListener(){
             override fun onAdLoaded() {
-                adViewContainer.visibility = View.VISIBLE
-                loadingView?.visibility = View.GONE
+                Log.d(BANNER_LOG, "onAdLoaded: Ad Loaded.")
+            }
+            override fun onAdFailedToLoad(error: LoadAdError) {
+                Log.d(BANNER_LOG, "onAdFailedToLoad: Ad Failed to Load, error: ${error.message}")
+            }
+            override fun onAdImpression() {
+                Log.d(BANNER_LOG, "onAdImpression: Ad Impression")
+            }
+        }
+    }
+
+
+    /******************
+        preload ads
+     ********************/
+    fun showBannerAd(
+        activity: Activity,
+        adViewContainer: FrameLayout
+    ){
+        val bannerAd = getBannerAd()
+        if (bannerAd == null){
+            Log.d(BANNER_LOG, "showBannerAd: Banner Ad is null")
+            return
+        }
+
+        adViewContainer.apply {
+            layoutParams.apply {
+                width = adSize.getWidthInPixels(activity)
+                height = adSize.getHeightInPixels(activity)
+            }
+            requestLayout()
+        }
+
+        adViewContainer.removeAllViews()
+        adViewContainer.addView(bannerAd)
+    }
+
+    private fun getBannerAd(): AdView?{
+        if (adViewList.isEmpty()){
+            Log.d(BANNER_LOG, "getBannerAd: Ad list is empty.")
+            return null
+        }
+        val size = adViewList.size
+        Log.d(BANNER_LOG, "getBannerAd: Ad list size: $size")
+        return adViewList.removeAt(size-1)
+    }
+
+    fun getPreBannerAds(
+        ads: Int,
+        activity: Activity,
+        adUnitId: String
+    ){
+        for (i in 1..ads){
+            val adView = createAdView(activity, adUnitId)
+            adViewList.add(adView)
+        }
+    }
+
+    private fun createAdView(
+        activity: Activity,
+        adUnitId: String
+    ): AdView{
+        val adView = AdView(context)
+        if (!::adSize.isInitialized){
+            adSize = getAdSize(activity, null)
+        }
+        adView.setAdSize(adSize)
+        adView.adUnitId = adUnitId
+
+        val adRequest = AdRequest.Builder().build()
+        adView.loadAd(adRequest)
+
+        adView.adListener = object: AdListener(){
+            override fun onAdLoaded() {
+                Log.d(BANNER_LOG, "onAdLoaded: Ad Loaded.")
+            }
+            override fun onAdFailedToLoad(error: LoadAdError) {
+                Log.d(BANNER_LOG, "onAdFailedToLoad: Ad Failed to Load, error: ${error.message}")
+            }
+            override fun onAdImpression() {
+                Log.d(BANNER_LOG, "onAdImpression: Ad Impression")
             }
         }
 
+        return adView
     }
 
 
